46fd2a3731ce0f496726d437d9a378d56391cbed
Kenneth Ward Jr. <kenwardjr8592@gmail.com>
2025-08-26 22:23:48 -0700

    Assignment 6: add updateTodo handler and thread onUpdateTodo through TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 import "./App.css";
-import TodoForm from "./TodoForm";
-import TodoList from "./TodoList";
+import TodoForm from "./features/TodoForm.jsx";
+import TodoList from "./features/TodoList/TodoList.jsx";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [title, setTitle] = useState("");
 
-  // Add a new todo with isCompleted = false
-  function addTodo(title) {
-    const newTodo = { id: Date.now(), title, isCompleted: false };
+ 
+  function addTodo(newTitle) {
+    const newTodo = { id: Date.now(), title: newTitle, isCompleted: false };
     setTodoList(prev => [...prev, newTodo]);
   }
 
-  // Mark a todo as completed
+ 
   function completeTodo(id) {
     const updatedTodos = todoList.map(todo =>
-      todo.id === id ? { ...todo, isCompleted: true } : todo
+      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+    );
+    setTodoList(updatedTodos);
+  }
+
+  
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map(todo =>
+      todo.id === editedTodo.id ? { ...editedTodo } : todo
     );
     setTodoList(updatedTodos);
   }
@@ -23,8 +32,12 @@ function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoForm onAddTodo={addTodo} title={title} setTitle={setTitle} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,6 @@
-import TodoListItem from "./TodoListItem";
+import TodoListItem from "./TodoListItem.jsx";
 
-function TodoList({ todoList, onCompleteTodo }) {
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
   // Filter out completed todos
   const filteredTodoList = todoList.filter(todo => !todo.isCompleted);
 
@@ -15,6 +15,7 @@ function TodoList({ todoList, onCompleteTodo }) {
               key={todo.id}
               todo={todo}
               onCompleteTodo={onCompleteTodo}
+              onUpdateTodo={onUpdateTodo}   {/* 👈 added */}
             />
           ))}
         </ul>
